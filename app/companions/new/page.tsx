6927dfc0fac21d94d3e22e803e75f13b18fb2bf8
sachin <sachinprvt5@gmail.com>
2025-06-22 00:27:27 +0530

    Impliment middlewares , subscription model , cleark billing , cleark auth and UI Update

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import CompanionForm from '@/components/CompanionForm'
 
 const NewCompanionPage = () => {
   return (
-    <div>
-      <h1>New Companion</h1>
-    </div>
+    <main className='in-lg-w-1/3 min-md:w-2/3 items-center justify-center'>
+
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Companion Builder</h1>
+        <CompanionForm/>
+      </article>
+    </main>
   )
 }
 
