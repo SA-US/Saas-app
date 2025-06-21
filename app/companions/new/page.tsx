e60432eb0b999f0568c984ba4e46804d8921a768
sachin <sachinprvt5@gmail.com>
2025-06-22 02:54:28 +0530

    Impliment Update

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import CompanionForm from '@/components/CompanionForm'
 
-const NewCompanionPage = () => {
+const NewCompanionPage = async () => { 
+  const {userId} = await auth() ; 
+  if(!userId) redirect('/sign-in')
+      
   return (
     <main className='in-lg-w-1/3 min-md:w-2/3 items-center justify-center'>
 
